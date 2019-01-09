2241ffa92ccd86c80ef9bd7447e04588f2375b9b
nvakulenko <natasha.vakulenko@gmail.com>
2019-01-09 11:27:18 +0200

    Add liveness and readiness probes, so now the greeting chart is correctly displayed on dashboard

--- src/main/java/com/lohika/GreetingController.java
+++ src/main/java/com/lohika/GreetingController.java
@@ -16,4 +16,9 @@ public class GreetingController {
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, name));
     }
+
+    @RequestMapping("/greeting/health")
+    public String health() {
+        return "health is ok";
+    }
 }
